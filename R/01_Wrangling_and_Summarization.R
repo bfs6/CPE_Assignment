efe55070dc06940f855ce1c9376f0a5a665ff212
bfs6 <basilfseif@gmail.com>
2020-05-13 01:32:04 -0400

    Part 1 Wrangling and Summarization

--- R/01_Wrangling_and_Summarization.R
+++ R/01_Wrangling_and_Summarization.R
@@ -1,6 +1,6 @@
 ####Read in Libraries####
 library(data.table)
-library(tidyverse)
+library(tidyverse) 
 library(dplyr)
 library(Hmisc)
 
@@ -10,6 +10,19 @@ pedestrian_stops <- fread("data-raw/pedestrian_stops.csv", sep = ",", header = T
 police_stop_data <- fread("data-raw/police_stop_data.csv", sep = ",", header = TRUE)
 police_use_of_force <- fread("data-raw/police_use_of_force.csv", sep = ",", header = TRUE)
 neighborhood_crime_stats <- fread("data-raw/neighborhood_crime_stats.csv", sep = ",", header = TRUE)
+
+
+####Part 1####
+police_use_of_force_tidy <-
+  police_use_of_force %>% 
+  select(CaseNumber, SubjectRoleNumber, ForceType) %>% 
+  distinct() %>%
+  group_by(CaseNumber, SubjectRoleNumber) %>%
+  mutate(ForceTypeNum = 1:n(), varType = "ForceType") %>%
+  unite(combi, varType, ForceTypeNum) %>% 
+  spread(combi, ForceType)
+number_of_police_force_incidents <- nrow(police_use_of_force_tidy) 
+  
 
 
 
